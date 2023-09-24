8e4715a80dc04a7d51cf338860b95b06986be018
sidhart25 <navneetnishchal2014@gmail.com>
2023-09-24 07:50:19 +0530

    terraform code checked

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "AKIASCUJSXQQ5EDCK2RG"
-  secret_key = "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"
+  access_key = "AKIASCUJSXQQ572XBH4O"
+  secret_key = "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"
 }
